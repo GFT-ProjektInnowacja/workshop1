8e4b83b89c1850bf71a4e4663fcd8c85e43d6572
patrykgitner <pgitner@gmail.com>
2020-11-05 18:01:17 +0100

    Dodanie metod GET, PUT oraz DELETE

--- src/main/java/com/workshop/controller/BookController.java
+++ src/main/java/com/workshop/controller/BookController.java
@@ -1,22 +1,49 @@
 package com.workshop.controller;
 
 import com.workshop.dto.CreateNewBookRequest;
+import com.workshop.entity.Book;
 import com.workshop.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/books")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @PostMapping(path = "/books/create")
+    @PostMapping(path = "/create")
     public ResponseEntity<String> createNewBook(@RequestBody CreateNewBookRequest request) {
         String result = bookService.createNewBook(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(path = "/get/{bookId}")
+    public ResponseEntity<Book> getBookById(@PathVariable(name = "bookId") Long bookId) {
+        Book book = bookService.getBookById(bookId);
+        return ResponseEntity.ok(book);
+    }
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<Book>> getAllBooksWrittenBySpecificAuthor(@RequestParam(name = "bookAuthor") String bookAuthor) {
+        List<Book> books = bookService.getAllBooksWrittenBySpecificAuthor(bookAuthor);
+        return ResponseEntity.ok(books);
+    }
+
+    @PutMapping(path = "/updatePages/{newNumberOfPages}/{bookId}")
+    public ResponseEntity<String> updateNumberOfPagesForSpecificBook(@PathVariable(name = "newNumberOfPages") int numberOfPages,
+                                                                     @PathVariable(name = "bookId") Long bookId) {
+        String result = bookService.updateNumberOfPages(numberOfPages, bookId);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping(path = "/delete/{bookId}")
+    public ResponseEntity<String> deleteSpecificBookById(@PathVariable(name = "bookId") Long bookId) {
+        String result = bookService.deleteSpecificBookById(bookId);
         return ResponseEntity.ok(result);
     }
 
--- src/main/java/com/workshop/service/BookService.java
+++ src/main/java/com/workshop/service/BookService.java
@@ -2,12 +2,15 @@ package com.workshop.service;
 
 import com.workshop.dto.CreateNewBookRequest;
 import com.workshop.entity.Book;
+import com.workshop.exceptions.BookNotFoundException;
 import com.workshop.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -27,6 +30,53 @@ public class BookService {
         bookRepository.save(book);
 
         return "Pomyślnie dodano nowa ksiazke do bazy danych!";
+    }
+
+    public Book getBookById(Long bookId) {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (!book.isPresent()) {
+            throw new BookNotFoundException("Książka o nr id: " + bookId + " nie istnieje!");
+        }
+
+        return book.get();
+    }
+
+    public List<Book> getAllBooksWrittenBySpecificAuthor(String bookAuthor) {
+        List<Book> books = bookRepository.getAllBooksWrittenBySpecificUser(bookAuthor);
+
+        if (books.isEmpty()) {
+            throw new BookNotFoundException("Autor " + bookAuthor + " nie posiada żadnych książek!");
+        }
+
+        return books;
+    }
+
+    public String updateNumberOfPages(int numberOfPages, Long bookId) {
+        Optional<Book> bookBeforeUpdate = bookRepository.findById(bookId);
+
+        if (!bookBeforeUpdate.isPresent()) {
+            throw new BookNotFoundException("Książka o nr id: " + bookId + " nie istnieje!");
+        }
+
+        Book updatedBook = bookBeforeUpdate.get();
+        updatedBook.setNumberOfPages(numberOfPages);
+
+        bookRepository.save(updatedBook);
+
+        return "Pomyślnie zaktualizowano liczbę stron na " + numberOfPages;
+    }
+
+    public String deleteSpecificBookById(Long bookId) {
+        Optional<Book> bookToDelete = bookRepository.findById(bookId);
+
+        if (!bookToDelete.isPresent()) {
+            throw new BookNotFoundException("Książka o nr id: " + bookId + " nie istnieje!");
+        }
+
+        bookRepository.delete(bookToDelete.get());
+
+        return "Pomyślnie usunięto książkę o tytule " + bookToDelete.get().getBookName();
     }
 
 }
